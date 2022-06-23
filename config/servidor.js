454c3e8934dc3a1368497a5b1b0fe51ba6fa58e4
Vallejo-05 <84139425+Vallejo-05@users.noreply.github.com>
2022-06-23 09:16:14 -0300

    Update servidor.js

--- config/servidor.js
+++ config/servidor.js
@@ -3,7 +3,7 @@ const express = require('express')
 //executar o express
 const app = express()
 //definir a porta do servidor local
-const porta = 4040
+const porta = process.env.PORT
 
 //definir a pasta dos arquivos estaticos (css, imagens, jquery)
 app.use(express.static('./assets'))
